b5dfea57316707efee380a13905b863a67c29848
PythonSnake66 <159868071+PythonSnake66@users.noreply.github.com>
2024-02-13 05:10:56 +0000

    Create q1.c

---
+++ C/q1.c
@@ -0,0 +1,9 @@
+// 1. Write a program which reads in two characters and outputs them in reverse order
+
+#include <stdio.h>
+int main() {
+    char a, b;
+    scanf(" %c %c", &a, &b);
+    printf("%c %c", b, a);
+    return 0;
+}
