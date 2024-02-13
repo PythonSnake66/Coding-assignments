e2f9bc1cc6df2840521d6a0eada4b85e473b2f75
PythonSnake66 <159868071+PythonSnake66@users.noreply.github.com>
2024-02-13 05:14:12 +0000

    Add files via upload

---
+++ C/q7a/q5header.h
@@ -0,0 +1,8 @@
+#ifndef Q5HEADER_H
+#define Q5HEADER_H
+#include <stdio.h>
+
+void simplify(int numerator, int denominator, int *sNumerator, int *sDenominator);
+void check(int numerator, int denominator, int sNumerator, int sDenominator);
+
+#endif
